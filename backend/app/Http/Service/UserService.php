0adc1dedb07d6516f5fc6018223a60ea899b891f
banli <244551048@qq.com>
2018-07-18 22:52:36 +0800

    add user

--- backend/app/Http/Service/UserService.php
+++ backend/app/Http/Service/UserService.php
@@ -14,7 +14,12 @@ class UserService {
 
     public $users = [
         'banli' => ['id' => 1, 'password' => 123, 'name' => 'banli', 'avatar' => 'https://raw.githubusercontent.com/taylorchen709/markdown-images/master/vueadmin/user.png'],
-        'selena' => ['id' => 2, 'password' => 123, 'name' => 'selena', 'avatar' => 'https://raw.githubusercontent.com/taylorchen709/markdown-images/master/vueadmin/user.png']
+        'selena' => ['id' => 2, 'password' => 123, 'name' => 'selena', 'avatar' => 'https://raw.githubusercontent.com/taylorchen709/markdown-images/master/vueadmin/user.png'],
+        'vip101' => ['id' => 3, 'password' => 'z38p53', 'name' => 'vip101', 'avatar' => 'https://raw.githubusercontent.com/taylorchen709/markdown-images/master/vueadmin/user.png'],
+        'vip102' => ['id' => 4, 'password' => '33bj62', 'name' => 'vip102', 'avatar' => 'https://raw.githubusercontent.com/taylorchen709/markdown-images/master/vueadmin/user.png'],
+        'vip103' => ['id' => 5, 'password' => 'k22i33', 'name' => 'vip103', 'avatar' => 'https://raw.githubusercontent.com/taylorchen709/markdown-images/master/vueadmin/user.png'],
+        'vip104' => ['id' => 6, 'password' => '38y94w', 'name' => 'vip104', 'avatar' => 'https://raw.githubusercontent.com/taylorchen709/markdown-images/master/vueadmin/user.png'],
+        'vip103' => ['id' => 7, 'password' => '63g99d', 'name' => 'vip105', 'avatar' => 'https://raw.githubusercontent.com/taylorchen709/markdown-images/master/vueadmin/user.png'],
     ];
 
     public function initUser($userId){
